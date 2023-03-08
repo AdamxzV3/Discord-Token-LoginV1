2b064aa9eaf6394fafa9eb711844c9da1a707e94
Adame <Adame@DESKTOP-TGU2OG3>
2023-03-08 09:31:17 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string token = "<YOUR_DISCORD_TOKEN_HERE>";
+
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("start-maximized");
+
+            IWebDriver driver = new ChromeDriver(options);
+
+            driver.Navigate().GoToUrl("https://discord.com/login");
+
+            IWebElement tokenInput = driver.FindElement(By.Name("email"));
+            tokenInput.SendKeys(token);
+
+            IWebElement loginButton = driver.FindElement(By.XPath("//button[@type='submit']"));
+            loginButton.Click();
+
+            Console.WriteLine(driver.PageSource);
+
+            string path = @"login_info.txt";
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine(driver.PageSource);
+            }
+
+            driver.Quit();
+        }
+    }
+}
